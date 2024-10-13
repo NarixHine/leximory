30ba55db59685455482bda0745feef34fd55699e
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2024-10-13 13:03:12 +0800

    Update bell.tsx

--- app/daily/bell.tsx
+++ app/daily/bell.tsx
@@ -10,23 +10,23 @@ export default function Bell({ hasSubscribed }: {
     hasSubscribed: boolean
 }) {
     const subscribe = async () => {
-        navigator.serviceWorker.register('/sw.js').then((register) => {
-            register.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY
-            }).then((subscription) => {
-                saveSubs(subscription)
-            }).catch(() => {
-                toast.error('失败')
-            })
-        }).catch(() => {
-            toast.error(isIOS() ? 'iOS 用户请将 Leximory 添加至主界面' : '失败')
+        const register = await navigator.serviceWorker.register('/sw.js')
+
+        const subscription = await register.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY
         })
+
+        await saveSubs(subscription)
     }
 
     return (
         <div className='flex flex-col justify-center items-center space-y-1'>
-            <Button variant={hasSubscribed ? 'solid' : 'ghost'} onPress={hasSubscribed ? () => delSubs() : () => subscribe()} size='lg' radius='full' color='primary' startContent={<PiClockClockwiseDuotone />}>{`${hasSubscribed ? '关闭' : '开启'} 22:00 日报提醒`}</Button>
+            <Button variant={hasSubscribed ? 'solid' : 'ghost'} onPress={hasSubscribed ? () => delSubs() : () => {
+                subscribe()
+                    .then(() => toast.success('开启成功'))
+                    .catch(() => toast.error(`开启失败${isIOS() ? '，iOS 用户请将 Leximory 添加至主界面' : ''}`))
+            }} size='lg' radius='full' color='primary' startContent={<PiClockClockwiseDuotone />}>{`${hasSubscribed ? '关闭' : '开启'} 22:00 日报提醒`}</Button>
             <div className='opacity-50 text-sm text-balance text-center'>
                 更换设备后需要重新开启
             </div>
