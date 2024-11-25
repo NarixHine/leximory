7850b8eafad4096054c4c4ee44a3278d71a2a57d
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2024-11-25 23:01:53 +0800

    Fixed

--- app/api/save/route.ts
+++ app/api/save/route.ts
@@ -15,9 +15,9 @@ const schema = z.object({
 })
 
 export async function POST(request: Request) {
-    const { lib, token, word: original } = await parseBody(request, schema)
+    const { lib, token, word: rawWord } = await parseBody(request, schema)
 
-    const word = `{{${lang === 'en' ? originals(word)[0] : word}}}`
+    const word = `{{${lang === 'en' ? originals(rawWord)[0] : rawWord}}}`
     validateOrThrow(word)
     const { sub } = await verifyToken(token, { secretKey: process.env.CLERK_SECRET_KEY! })
     const { lang } = await authWriteToLib(lib, sub)
