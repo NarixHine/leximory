2a42121016eba8071e3f1021b4a744b5c6147b01
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2025-02-05 22:51:19 +0800

    Update expiration

--- app/library/components/follow-us/actions.tsx
+++ app/library/components/follow-us/actions.tsx
@@ -4,5 +4,7 @@ import { cookies } from 'next/headers'
 
 export async function showFollowUsModal() {
     const cookieStore = await cookies()
-    cookieStore.set('has-shown-follow-us-modal', 'true')
+    cookieStore.set('has-shown-follow-us-modal', 'true', {
+        maxAge: Number.MAX_SAFE_INTEGER,
+    })
 }
