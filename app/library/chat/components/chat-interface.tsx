029cf63a0438e80385eda43aebef7abf3a484b8a
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2025-06-01 10:56:08 +0800

    Update chat-interface.tsx

--- app/library/chat/components/chat-interface.tsx
+++ app/library/chat/components/chat-interface.tsx
@@ -402,7 +402,7 @@ export default function ChatInterface({ plan }: { plan: Plan }) {
 
     return (
         <Main style={{ fontFamily: chatFontFamily }} className='flex flex-col max-w-2xl'>
-            <div className='flex justify-between items-center mb-4'>
+            <div className='flex justify-between items-center mb-4 sticky top-10 z-10 backdrop-blur bg-background/50'>
                 <H usePlayfair className={'text-3xl bg-gradient-to-r from-primary-600 to-primary-400 bg-clip-text text-transparent'}>
                     Talk to Your Library
                 </H>
@@ -457,7 +457,7 @@ export default function ChatInterface({ plan }: { plan: Plan }) {
             <div ref={messagesEndRef} />
             <form
                 onSubmit={handleFormSubmit}
-                className='flex items-center gap-2 sticky bottom-10'
+                className='flex items-center gap-2'
             >
                 <input
                     type='file'
