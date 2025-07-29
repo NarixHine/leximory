aa8bb959fa7bf82b88c56da83c975fcac652ec6d
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2025-07-29 18:29:10 +0800

    Update Memories page UI and fix StreakSkeleton height
    Enhanced the Memories page with additional explanatory text, improved font usage, and added spacing for better layout. Also fixed the height values in the StreakSkeleton component to correct the responsive design.

--- app/memories/page.tsx
+++ app/memories/page.tsx
@@ -5,8 +5,9 @@ import { MemoryTabs } from './components/memory-tabs'
 import Main from '@/components/ui/main'
 import H from '@/components/ui/h'
 import { cn } from '@/lib/utils'
-import { ENGLISH_FANCY } from '@/lib/fonts'
+import { contentFontFamily, ENGLISH_FANCY } from '@/lib/fonts'
 import { Metadata } from 'next'
+import { Spacer } from '@heroui/spacer'
 
 export const metadata: Metadata = { title: 'Memories' }
 
@@ -18,8 +19,14 @@ export default function MemoriesPage() {
             <div className='flex flex-col md:flex-row-reverse gap-8'>
                 <section className='md:basis-1/3 items-center flex flex-col'>
                     <H fancy className='text-5xl lg:text-6xl hidden md:block'>Memories</H>
-                    <span className={cn('italic hidden md:block text-xl lg:text-2xl mb-4 text-default-700', ENGLISH_FANCY.className)}>Record your learnings</span>
+                    <div className={cn('italic hidden md:block text-xl lg:text-2xl mb-4 text-default-700', ENGLISH_FANCY.className)}>Record your learnings</div>
                     <CreateMemoryForm />
+                    <Spacer y={4} />
+                    <div style={{ fontFamily: contentFontFamily }} className='text-sm text-default-600 text-balance text-center max-w-52'>
+                        Memories 是一个日记本功能。
+                        <br></br>
+                        打卡 Memory 会在设置中显示连续打卡天数（Streak）。
+                    </div>
                 </section>
                 <section className='md:basis-full w-full justify-start flex'>
                     <MemoryTabs
