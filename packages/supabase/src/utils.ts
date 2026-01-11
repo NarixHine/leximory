d23577e4e5856b587756bb6b936f83e62c11c790
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2026-01-11 16:18:45 +0800

    Refactor cookiesFactory for production and dev environments
    Updated cookiesFactory to return a specific cookie configuration only in production, including a fixed name, domain, and secure flag. In non-production environments, it now returns an empty object.

--- packages/supabase/src/utils.ts
+++ packages/supabase/src/utils.ts
@@ -1,14 +1,21 @@
+// cookieFactory.ts
 import { IS_PROD } from '@repo/env'
 import { prefixUrl } from '@repo/env/config'
 import { seconds } from 'itty-time'
 
 export function cookiesFactory() {
-    const authDomain = new URL(prefixUrl('/')).hostname
-    return {
-        name: `sb-${authDomain}-auth-token`,
-        ...(IS_PROD && { domain: `.${authDomain}` }),
-        path: '/',
-        maxAge: seconds('1 year'),
-        sameSite: 'lax' as const,
+    if (IS_PROD) {
+        const domain = new URL(prefixUrl('/')).hostname
+        return {
+            name: `leximory-auth-token`,
+            domain,
+            path: '/',
+            maxAge: seconds('1 year'),
+            sameSite: 'lax' as const,
+            secure: true,
+        }
+    }
+    else {
+        return {}
     }
 }
